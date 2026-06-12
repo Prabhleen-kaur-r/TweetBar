50fd68fce16a1a3c915244592e1b851e78f46ee2
Prabhleen-kaur-r <rkaurprabhleen@gmail.com>
2026-06-12 19:31:09 +0530

    deployment settings

--- twitterapp/settings.py
+++ twitterapp/settings.py
@@ -30,8 +30,7 @@ SECRET_KEY = os.getenv("DJANGO_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv("DEBUG", "False") == "True"
 
-ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "localhost,127.0.0.1,.onrender.com").split(",")
-
+ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "localhost,127.0.0.1,tweetbar-8tv5.onrender.com,.onrender.com").split(",")
 
 # Application definition
 
